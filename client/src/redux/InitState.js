c4fa158a07b20f221954e21c0d18bbc2ec18e98d
bzelijah <mayzzze@gmail.com>
2021-02-26 17:15:43 +0300

    fix

--- client/src/redux/InitState.js
+++ client/src/redux/InitState.js
@@ -1,18 +1,17 @@
 const initState = {
   game: [{
-    id:1,
+    id: 1,
     theme: 'Спорт',
     questions: [
       {
-        id:11,
+        id: 11,
         points: 200,
         question: 'Что такое спорт?',
         answer: 'Это жизнь',
         status: false,
       },
       {
-        id:12,
-
+        id: 12,
         points: 400,
         questions: 'Когда придумали футбол?',
         asnwer: 'В 19 веке',
@@ -21,15 +20,18 @@ const initState = {
     ],
   },
   {
+    id: 2,
     theme: 'Загадки',
     questions: [
       {
+        id: 21,
         points: 200,
         question: 'Какой рукой лучше размешивать чай?',
         answer: 'Чай лучше размешивать ложкой',
         status: false,
       },
       {
+        id: 22,
         points: 400,
         question: 'Что может быть больше слона и одновременно невесомым?',
         answer: 'Тень слона',
@@ -38,15 +40,18 @@ const initState = {
     ],
   },
   {
+    id: 3,
     theme: 'Разминка',
     questions: [
       {
+        id: 31,
         points: 200,
         question: 'Что такое свиная отбивная?',
         answer: 'Картошка, отбитая у свиньи',
         status: false,
       },
       {
+        id: 32,
         points: 400,
         question: 'Кто впеpвые стал пpиглашать учёных на "Огонек"?',
         answer: 'Инквизиторы...',
@@ -56,6 +61,7 @@ const initState = {
   },
   ],
   users: [{
+    id: 1,
     login: 'Igor',
     password: 'tapac',
     points: 0,
